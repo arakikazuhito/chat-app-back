6ba9f4a938e2833ad47d204bcfa76bb55374b60a
arakikazuhito <k.araki.carecon@gmail.com>
2024-06-07 14:15:59 +0900

    修正

--- nginx/src/work/app/Http/Controllers/ChatController.php
+++ nginx/src/work/app/Http/Controllers/ChatController.php
@@ -8,7 +8,7 @@ class ChatController extends Controller
 {
     public function show()
     {
-        $htmlContent = file_get_contents(resource_path('views/test.html'));
+        $htmlContent = file_get_contents('/var/www/resources/views/test.html');
         return response($htmlContent)->header('Content-Type', 'text/html');
     }
 }
